42413fe1db26a73cb6db413d9e6979579e657c92
Diego <diefusaro@gmail.com>
2023-09-26 00:29:24 -0300

    code: structure and styles finished

---
+++ src/components/About.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
+
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className='xs:w-[250px] w-full'>
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450
+          }}
+          className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+        >
+          <img src={icon} alt={title} className='w-16 h-16 object-contain'/>
+          <h3 className='text-white text-[20px] font-bold text-center' >{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  )
+}
+
+const About = () => {
+  return (
+    <>
+      <motion.div
+        variants={textVariant()}
+      >
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        Greetings! My name is Diego Fusaro and I'm a Web Full-Stack Developer. I have experience in TypeScript, JavaScript and Kotlin with CSS compresors like SASS and Framer-Motion. I have expertise in frameworks like ReactJS, NodeJS, React Native, TailwindCSS and now I'm learning Three.js. I'm a curious person and everytime I face a problem, I take it personal and make the best that I can to resolve it and become better in what I do. Let's work together!
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10 justify-center'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        )) }
+      </div>
+    </>
+  )
+}
+
+export default SectionWrapper(About, "about");
---
+++ src/components/Works.jsx
@@ -0,0 +1,108 @@
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { github, netlify } from "../assets";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ProjectCard = ({
+  index,
+  name,
+  description,
+  tags,
+  image,
+  source_code_link,
+  source_code_link1
+}) => {
+  return (
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+      <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+      >
+        <div className="relative w-full h-[230px]">
+          <img
+            src={image}
+            alt={name}
+            className="w-full h-full object-cover rouded-2xl"
+          />
+
+          <div className="absolute inset-0 flex justify-end m-3 card-image_hover">
+            <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <img
+                src={github}
+                alt={github}
+                className="w-1/2 h-1/2 object-contain"
+              />
+            </div>
+            <div
+              onClick={() => window.open(source_code_link1, "_blank")}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <img
+                src={netlify}
+                alt={netlify}
+                className="w-1/2 h-1/2 object-contain"
+              />
+            </div>
+          </div>
+          
+        </div>
+
+        <div className="mt-5">
+          <h3 className="text-white font-bold text-[24px]">{name}</h3>
+          <p className="mt-2 text-secondary text-[14px]">{description}</p>
+        </div>
+
+        <div className="mt-2 flex flex-wrap gap-2">
+          {tags.map((tag) => (
+            <p key={tag.name} className={`text-[14px] ${tag.color}`}>
+              #{tag.name}
+            </p>
+          ))}
+        </div>
+      </Tilt>
+    </motion.div>
+  );
+};
+
+const Works = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>My client's satisfaction</p>
+        <h2 className={styles.sectionHeadText}>Projects</h2>
+      </motion.div>
+
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
+          The following projects highlight my expertise and experience by
+          presenting real-world illustrations of my work. Each project comes
+          with concise descriptions and links to code repositories and live
+          demonstrations. They serve as a testament to my capability in tackling
+          intricate challenges, adapting to diverse technologies, and
+          efficiently overseeing project execution.
+        </motion.p>
+      </div>
+
+      <div className="mt-20 flex flex-wrap gap-7 justify-center">
+        {projects.map((project, index) => (
+          <ProjectCard key={`project-${index}`} index={index} {...project} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(Works, "");
